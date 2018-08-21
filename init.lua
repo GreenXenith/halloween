81d95ecddd1f8984dd58468930c48c51a7127fce
GreenXenith <bl00ntdmonkey@yahoo.com>
2018-08-20 18:08:16 -0700

    Update to new 3d_armor api. Overhaul API. Add cat mask.

--- init.lua
+++ init.lua
@@ -1,71 +1,89 @@
 -- Costumes
 -- Original Skin Texture Credit: Original skin authors (whoever they may be)
 
-local costume = {}
-
-local level = 0
-local heal = 0
-local use = 0
-local fire = 0
-local water = 0
-local radiation = 0
-local wear = 0
-
 -- Function for registering suits
-function costume.register(costume, nodename, original_def)
-	minetest.register_tool("halloween:suit_"..nodename, {
-		description = costume.." Costume",
-		inventory_image = "inv_suit_"..nodename..".png",
-		groups = {
-			armor_head = level,
-			armor_torso = level,
-			armor_legs = level,
-			armor_feet = level,
-			armor_heal = heal,
-			armor_use = use,
-			armor_fire = fire,
-			armor_water = water,
-			armor_radiation = radiation,
-		},
-		wear = 0,
+local function register_costume(costume, nodename, def)
+
+	local gen_prev = "((([combine:16x32:-16,-12=halloween_suit_" .. nodename .. ".png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_suit_" .. nodename .. ".png^[mask:mask_head.png)^([combine:16x32:-44,-12=halloween_suit_" .. nodename .. ".png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-44,-12=halloween_suit_" .. nodename .. ".png^[mask:mask_arm.png)^([combine:16x32:4,0=halloween_suit_" .. nodename .. ".png^[mask:mask_leg.png^[transformFX)^([combine:16x32:4,0=halloween_suit_" .. nodename .. ".png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
+
+	local grouptypes = {
+		["suit"] = {armor_head=1, armor_torso=1, armor_legs=1, armor_feet=1, armor_use=1000},
+		["mask"] = {armor_head=1, armor_use=1000},
+		["shirt"] = {armor_torso=1, armor_use=1000},
+	}
+
+	local function is_type(key)
+		return grouptypes[key] ~= nil
+	end
+
+	local grouptype = "suit"
+
+	if def then
+		if def.type then
+			if is_type(def.type) then
+				grouptype = def.type
+			end
+		end
+	end
+
+	local armor_groups = grouptypes[grouptype]
+	local node_str = grouptype.."_"..nodename
+
+	if def then
+		if def.name then
+			node_str = def.name
+		end
+	end
+
+	armor:register_armor("halloween:"..node_str, {
+		description = costume,
+		inventory_image = "inv_"..grouptype.."_"..nodename..".png",
+		groups = armor_groups
+	})
+
+	local override_prev = gen_prev
+	if def then
+		if def.preview then
+			override_prev = def.preview
+		end
+	end
+
+	minetest.override_item("halloween:"..node_str, {
+		preview = override_prev
 	})
 end
 
 -- Register Costumes
-costume.register("Devil (Female)", "devil_fe")
-costume.register("Devil (Male)", "devil_ma")
-costume.register("Dinosaur", "dino")
-costume.register("Frankenstein", "frank")
-costume.register("Ghost", "ghost")
-costume.register("Grim Reaper", "reaper")
-costume.register("Killer", "killer")
-costume.register("Glow-in-the-dark Skeleton", "skeleton")
-costume.register("Unicorn", "unicorn")
-costume.register("Vampire", "vampire")
-costume.register("Wearwolf", "wearwolf")
-
--- Other Apparel
-minetest.register_tool("halloween:helmet_pumpkin_mask", {
-		description = "Pumpkin Mask",
-		inventory_image = "inv_helmet_pumpkin_mask.png",
-		groups = {
-			armor_head = level,
-			armor_heal = heal,
-			armor_use = use,
-		},
-		wear = 0,
+register_costume("Devil (Female)", "devil_fe", {
+	preview = "((([combine:16x32:-28,-12=halloween_suit_devil_fe.png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_suit_devil_fe.png^[mask:mask_head.png)^([combine:16x32:-48,-12=halloween_suit_devil_fe.png^[mask:mask_arm.png)^([combine:16x32:-48,-12=halloween_suit_devil_fe.png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-52,0=halloween_suit_devil_fe.png^[mask:mask_leg.png^[transformFX)^([combine:16x32:-52,0=halloween_suit_devil_fe.png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
+})
+register_costume("Devil (Male)", "devil_ma", {
+	preview = "((([combine:16x32:-28,-12=halloween_suit_devil_ma.png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_suit_devil_ma.png^[mask:mask_head.png)^([combine:16x32:-52,-12=halloween_suit_devil_ma.png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-52,-12=halloween_suit_devil_ma.png^[mask:mask_arm.png)^([combine:16x32:4,0=halloween_suit_devil_ma.png^[mask:mask_leg.png^[transformFX)^([combine:16x32:4,0=halloween_suit_devil_ma.png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
+})
+register_costume("Dinosaur", "dino")
+register_costume("Frankenstein", "frank")
+register_costume("Ghost", "ghost")
+register_costume("Grim Reaper", "reaper")
+register_costume("Killer", "killer")
+register_costume("Glow-in-the-dark Skeleton", "skeleton")
+register_costume("Unicorn", "unicorn")
+register_costume("Vampire", "vampire")
+register_costume("Wearwolf", "wearwolf")
+register_costume("Pumpkin Mask", "pumpkin", {
+	type = "mask",
+	preview = "((([combine:16x32:-16,-12=halloween_mask_pumpkin.png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_mask_pumpkin.png^[mask:mask_head.png)^([combine:16x32:-44,-12=halloween_mask_pumpkin.png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-44,-12=halloween_mask_pumpkin.png^[mask:mask_arm.png)^([combine:16x32:4,0=halloween_mask_pumpkin.png^[mask:mask_leg.png^[transformFX)^([combine:16x32:4,0=halloween_mask_pumpkin.png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
+})
+register_costume("Halloween Hoodie", "halloween_hoodie", {
+	type = "shirt",
+	preview = "((([combine:16x32:-28,-12=halloween_shirt_halloween_hoodie.png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_shirt_halloween_hoodie.png^[mask:mask_head.png)^([combine:16x32:-52,-12=halloween_shirt_halloween_hoodie.png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-52,-12=halloween_shirt_halloween_hoodie.png^[mask:mask_arm.png)^([combine:16x32:4,0=halloween_shirt_halloween_hoodie.png^[mask:mask_leg.png^[transformFX)^([combine:16x32:4,0=halloween_shirt_halloween_hoodie.png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
+})
+register_costume("Cat Mask", "cat", {
+	type = "mask",
+	preview = "((([combine:16x32:-16,-12=halloween_mask_cat.png^[mask:mask_chest.png)^([combine:16x32:-36,-8=halloween_mask_cat.png^[mask:mask_head.png)^([combine:16x32:-44,-12=halloween_mask_cat.png^[mask:mask_arm.png^[transformFX)^([combine:16x32:-44,-12=halloween_mask_cat.png^[mask:mask_arm.png)^([combine:16x32:4,0=halloween_mask_cat.png^[mask:mask_leg.png^[transformFX)^([combine:16x32:4,0=halloween_mask_cat.png^[mask:mask_leg.png))^[resize:32x64)^[mask:mask_preview.png"
 })
 
-minetest.register_tool("halloween:chestplate_halloween_hoodie", {
-		description = "Halloween Hoodie",
-		inventory_image = "inv_chestplate_halloween_hoodie.png",
-		groups = {
-			armor_torso = level, 
-			armor_heal = level, 
-			armor_use = use,
-		},
-		wear = 0,
-})
+minetest.register_alias("halloween:helmet_pumpkin_mask", "halloween:mask_pumpkin")
+minetest.register_alias("halloween:chestplate_halloween_hoodie", "halloween:shirt_halloween_hoodie")
 
 -- Candy
 
